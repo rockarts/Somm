b8d1376d36487b76447cac325602a36fcd4ef178
Steven Rockarts <stevenrockarts@gmail.com>
2022-03-21 07:22:50 -0600

    Initial Commit

---
+++ Somm/SommApp.swift
@@ -0,0 +1,17 @@
+//
+//  SommApp.swift
+//  Somm
+//
+//  Created by Steven Rockarts on 2022-03-21.
+//
+
+import SwiftUI
+
+@main
+struct SommApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
